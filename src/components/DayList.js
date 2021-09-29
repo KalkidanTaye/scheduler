f3acd937ddfccd242cae4ffb97f8f8f01b313a2f
Kalkidan Alemu <kali.taye@gmail.com>
2021-09-29 19:31:15 -0400

    add DayListItem feature and test

---
+++ src/components/DayList.js
@@ -0,0 +1,15 @@
+// export default function DayList(props) {
+
+//   return (
+//     day.map(day) => (
+//         <DayListItem
+
+//         name={day.name}
+//         spots={day.spots}
+//         selected={day.name === props.day}
+//         setDay={props.setDay}
+//       />
+//     )
+
+//   );
+// }
