5a624381c295fa9ba52f11b2c5d2314b00584ca5
Kalkidan Alemu <kali.taye@gmail.com>
2021-09-29 23:28:56 -0400

    add intervierListItem component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,14 +3,18 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
   const { days, day, setDay } = props;
-  const parsedDaysArray = days.map((parseDay) => (
-    <DayListItem
-      key={parseDay.id}
-      name={parseDay.name}
-      spots={parseDay.spots}
-      selected={parseDay.name === day}
-      setDay={setDay}
-    />
-  ));
+  let parsedDaysArray = [];
+  if (days) {
+    parsedDaysArray = days.map((parseDay) => (
+      <DayListItem
+        key={parseDay.id}
+        name={parseDay.name}
+        spots={parseDay.spots}
+        selected={parseDay.name === day}
+        setDay={setDay}
+      />
+    ));
+  }
+
   return <ul>{parsedDaysArray}</ul>;
 }
