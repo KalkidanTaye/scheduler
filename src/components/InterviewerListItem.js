b52765cfa9b5f6b43e809e21796d59cfe93ebb1c
Kalkidan Alemu <kali.taye@gmail.com>
2021-09-30 10:45:05 -0400

    Feature: add interviewer List Item function

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,20 @@
 import React from "react";
+import "components/interviewerListItem.scss";
+import classnames from "classnames";
 
 export default function Application(props) {
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 }
