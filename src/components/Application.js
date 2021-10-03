d5baa3e51a8e638e9a530de7e2450f2203acb618
Kalkidan Alemu <kali.taye@gmail.com>
2021-10-02 22:54:45 -0400

    Feature: Fetch Days API with axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment/index";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 1,
+    time: "2pm",
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Archie Cohen",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Maria Boucher",
+      interviewer: {
+        id: 2,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
+    },
+  },
+];
 
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState(["Monday"]);
+  const [days, setDays] = useState([]);
+  const mappedAppointments = appointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
+  useEffect(() => {
+    const daysURL = "http://localhost:8001/api/days";
+    axios.get(daysURL).then((response) => {
+      console.log(response.data);
+      setDays([...response.data]);
+    });
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -42,6 +85,8 @@ export default function Application(props) {
       <section className="schedule">
         <DayList />
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {mappedAppointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
